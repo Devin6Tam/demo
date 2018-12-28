faa19ec9f3bc2cb42ecd93ad9fd158e70cb5d11f
tanxw <tanxw@kaisagroup.com>
2018-12-28 14:03:42 +0800

    dubbo.xml 配置版本

--- demo-sample101/src/main/java/com/mzbloc/demo/sample101/DemoSample101Application.java
+++ demo-sample101/src/main/java/com/mzbloc/demo/sample101/DemoSample101Application.java
@@ -2,8 +2,10 @@ package com.mzbloc.demo.sample101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = {"classpath:/dubbo.xml"})
 public class DemoSample101Application {
 
 	public static void main(String[] args) {
