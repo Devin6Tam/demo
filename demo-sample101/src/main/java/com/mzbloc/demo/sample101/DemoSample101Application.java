f423ae5b8d263170e0d837feb336b537e7cac3c5
tanxw <tanxw@kaisagroup.com>
2018-12-28 12:00:15 +0800

    修改

--- demo-sample101/src/main/java/com/mzbloc/demo/sample101/DemoSample101Application.java
+++ demo-sample101/src/main/java/com/mzbloc/demo/sample101/DemoSample101Application.java
@@ -2,10 +2,8 @@ package com.mzbloc.demo.sample101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@ImportResource(value = {"classpath:dubbo.xml"})
 public class DemoSample101Application {
 
 	public static void main(String[] args) {
