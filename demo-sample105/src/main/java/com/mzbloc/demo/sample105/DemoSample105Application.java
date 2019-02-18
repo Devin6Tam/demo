58710ba8e43f82d788b692b0ad3e1a2d0b408c91
tanxw <tanxw@kaisagroup.com>
2019-02-18 10:46:22 +0800

    修改

---
+++ demo-sample105/src/main/java/com/mzbloc/demo/sample105/DemoSample105Application.java
@@ -0,0 +1,15 @@
+package com.mzbloc.demo.sample105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@SpringBootApplication
+public class DemoSample105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSample105Application.class, args);
+	}
+
+}
+
