7910ddd64c2ea0c9929a20a451cf14685d8b2b64
tanxw <tanxw@kaisagroup.com>
2019-06-25 15:01:42 +0800

    one more again

---
+++ demo-sample107/src/main/java/com/mzbloc/demo/sample107/DemoSample107Application.java
@@ -0,0 +1,13 @@
+package com.mzbloc.demo.sample107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSample107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSample107Application.class, args);
+	}
+
+}
